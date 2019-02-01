b30bb2517815b70a9415a71b8fc6449e0f7b91c3
Max Malyk <max.malyk@nuance.com>
2019-02-01 09:46:57 -0600

    removed "purchase recordsers" link, added js/css bundling, updated fontawesome hosted, UI tweaks
    Related Work Items: #28344

--- SoftwareSite/App_Start/RouteConfig.cs
+++ SoftwareSite/App_Start/RouteConfig.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
-namespace SoftwareSite
+namespace Emdat.SoftwareSite
 {
     public class RouteConfig
     {
--- SoftwareSite/Global.asax.cs
+++ SoftwareSite/Global.asax.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using Emdat.SoftwareSite;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace SoftwareSite
@@ -13,6 +11,7 @@ namespace SoftwareSite
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
