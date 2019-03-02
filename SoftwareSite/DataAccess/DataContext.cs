e5f3a1a47e271e144e27875833638e549f365acb
Chris Mielke <chris.mielke@nuance.com>
2019-03-01 18:35:26 -0600

    Shadowlink update

--- SoftwareSite/DataAccess/DataContext.cs
+++ SoftwareSite/DataAccess/DataContext.cs
@@ -128,9 +128,9 @@ namespace Emdat.SoftwareSite.DataAccess
                     {
                         new GetLatestApplicationVersionsRow
                         {
-                            InstallerFileName = "ShadowLinkSetup_2_8_19060_41188.exe",
-                            InstallerFileNameCanada = "ShadowLinkSetup_2_8_19060_41188.exe",
-                            InstallerFileNameUK = "ShadowLinkSetup_2_8_19060_41188.exe",
+                            InstallerFileName = "ShadowLinkSetup_2_8_19061_894.exe",
+                            InstallerFileNameCanada = "ShadowLinkSetup_2_8_19061_894.exe",
+                            InstallerFileNameUK = "ShadowLinkSetup_2_8_19061_894.exe",
                             MajorVersionNumber = "2",
                             ManualPdfFileName = "ShadowLink2%20User%20Guide_6_2016.pdf",
                             ManualWordFileName = "ShadowLink2%20User%20Guide_6_2016.docx",
